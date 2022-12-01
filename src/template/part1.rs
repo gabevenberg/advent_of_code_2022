ea7220bb0051b72638e4e2283cff157b29f40b7d
gabe <gabevenberg@gmail.com>
2022-11-30 22:56:59 -0600

    added template for AOC.

---
+++ src/template/part1.rs
@@ -0,0 +1,13 @@
+pub fn part1() -> usize {
+    return 0;
+}
+
+#[cfg(test)]
+mod day01 {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 0);
+    }
+}
---
+++ src/template/part2.rs
@@ -0,0 +1,13 @@
+pub fn part2() -> usize {
+    return 0;
+}
+
+#[cfg(test)]
+mod day01 {
+    use super::*;
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 0);
+    }
+}
