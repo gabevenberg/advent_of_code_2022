c92c8843e9ac62111363b2062c7b3d647b42e99c
gabe <gabevenberg@gmail.com>
2022-12-04 00:41:09 -0600

    finished day 4.

--- src/day04/part2.rs
+++ src/day04/part2.rs
@@ -1,7 +1,10 @@
 use crate::utilities::*;
 
-pub fn part2() -> usize {
-    0
+pub fn part2(input: &[(Range, Range)]) -> usize {
+    input
+        .iter()
+        .filter(|tuple| tuple.0.any_overlap(&tuple.1))
+        .count()
 }
 
 #[cfg(test)]
@@ -10,6 +13,15 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), 0);
+        let input = vec![
+            (Range::new(2, 4), Range::new(6, 8)),
+            (Range::new(2, 3), Range::new(4, 5)),
+            (Range::new(5, 7), Range::new(7, 9)),
+            (Range::new(2, 8), Range::new(3, 7)),
+            (Range::new(6, 6), Range::new(4, 6)),
+            (Range::new(2, 6), Range::new(4, 8)),
+            (Range::new(19, 30), Range::new(5, 18)),
+        ];
+        assert_eq!(part2(&input), 4);
     }
 }
--- src/day04/utilities.rs
+++ src/day04/utilities.rs
@@ -1,6 +1,65 @@
-pub fn parse(input: &str) -> usize {
-    println!("{}", input);
-    0
+use once_cell::sync::Lazy;
+use regex::Regex;
+#[derive(Debug, PartialEq, Eq)]
+pub struct Range {
+    start: u16,
+    end: u16,
+}
+
+impl Range {
+    pub fn new(start: u16, end: u16) -> Self {
+        Self {
+            start: start.min(end),
+            end: end.max(start),
+        }
+    }
+
+    pub fn calc_size(&self) -> u16 {
+        self.start.abs_diff(self.end)
+    }
+
+    pub fn any_overlap(&self, other: &Self) -> bool {
+        self.start <= other.end && self.end >= other.start
+    }
+
+    pub fn calc_overlap(&self, other: &Self) -> Range {
+        let overlap_start = self.start.min(other.start);
+        let overlap_end = self.end.max(other.end);
+        Range::new(overlap_start, overlap_end)
+    }
+
+    pub fn complete_overlap(&self, other: &Self) -> bool {
+        self.calc_overlap(other) == *self || self.calc_overlap(other) == *other
+    }
+
+    pub fn start(&self) -> u16 {
+        self.start
+    }
+
+    pub fn end(&self) -> u16 {
+        self.end
+    }
+}
+
+static PARSE_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)").unwrap());
+
+pub fn parse(input: &str) -> Vec<(Range, Range)> {
+    input
+        .lines()
+        .map(|line| {
+            let cap = PARSE_REGEX.captures(line).unwrap();
+            (
+                Range::new(
+                    cap.get(1).unwrap().as_str().parse().unwrap(),
+                    cap.get(2).unwrap().as_str().parse().unwrap(),
+                ),
+                Range::new(
+                    cap.get(3).unwrap().as_str().parse().unwrap(),
+                    cap.get(4).unwrap().as_str().parse().unwrap(),
+                ),
+            )
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -9,9 +68,24 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let input =
-"test"
-;
-        assert_eq!(parse(input), 0);
+        let input = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+19-30,5-18";
+        assert_eq!(
+            parse(input),
+            vec![
+                (Range::new(2, 4), Range::new(6, 8)),
+                (Range::new(2, 3), Range::new(4, 5)),
+                (Range::new(5, 7), Range::new(7, 9)),
+                (Range::new(2, 8), Range::new(3, 7)),
+                (Range::new(6, 6), Range::new(4, 6)),
+                (Range::new(2, 6), Range::new(4, 8)),
+                (Range::new(19, 30), Range::new(5, 18)),
+            ]
+        );
     }
 }
